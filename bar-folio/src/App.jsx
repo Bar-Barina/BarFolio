590516adeb800cc2b31d8dc55f61cbd40585464f
Bar Barina <itsbarbarina@gmail.com>
2024-05-12 19:21:52 +0300

    Added header component

--- bar-folio/src/App.jsx
+++ bar-folio/src/App.jsx
@@ -1,20 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../src/assets/styles/styles.scss'
+import Header from './Header'
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-    </>
-  )
+  return <Header />
 }
 
 export default App
