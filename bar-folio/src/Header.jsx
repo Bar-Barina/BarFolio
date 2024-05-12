c9e538a8cbccc7ea7c5dc26fade623e7b960d698
Bar Barina <itsbarbarina@gmail.com>
2024-05-12 19:46:58 +0300

    Added fonts and header content

--- bar-folio/src/Header.jsx
+++ bar-folio/src/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <section className='header-main-section flex space-between justify-center'>
+      <span className='logo'>Bar.dev</span>
+      <div className='nav-content'>
+        <span>About</span>
+        <span>Experience</span>
+        <span>Projects</span>
+        <span>Contact</span>
+      </div>
+    </section>
+  )
 }
 
 export default Header
