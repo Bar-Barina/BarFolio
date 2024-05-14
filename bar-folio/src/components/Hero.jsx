b1d0619ff30c1e20b8ea4d62b49ddac09b60f60c
Bar Barina <itsbarbarina@gmail.com>
2024-05-14 12:23:48 +0300

    changed hero layout

--- bar-folio/src/components/Hero.jsx
+++ bar-folio/src/components/Hero.jsx
@@ -4,49 +4,49 @@ import { getSvg } from '../services/svg.service'
 
 const Hero = () => {
   return (
-    <section className='hero-main-section flex auto-center'>
-      <div className='main-text-wrapper'>
-        <h1>
-          <span>Hi, I'm </span>
-          Bar Barina.
-        </h1>
-        <h1>Front-End Developer.</h1>
-        <p>
-          I build pixel-perfect, engaging, and accessible digital experiences.
-        </p>
-        <div className='blue-colored-div'></div>
-        <div className='lightblue-colored-div'></div>
-        <div className='links'>
-          <a
-            href='https://www.linkedin.com/in/bar-barina/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='pointer'
-            dangerouslySetInnerHTML={{
-              __html: getSvg('linkedin'),
-            }}
-          ></a>
-          <a
-            href='https://github.com/Bar-Barina'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='pointer'
-            dangerouslySetInnerHTML={{
-              __html: getSvg('github'),
-            }}
-          ></a>
+    <section className='hero-main-section'>
+      <section className='hero-main-wrapper flex auto-center'>
+        <div className='main-text-wrapper'>
+          <h1>
+            <span>Hi, I'm </span>
+            Bar Barina.
+          </h1>
+          <h1>Front-End Developer.</h1>
+          <p>
+            I build pixel-perfect, engaging, and accessible digital experiences.
+          </p>
+          <div className='blue-colored-div'></div>
+          <div className='lightblue-colored-div'></div>
+          <div className='links'>
+            <a
+              href='https://www.linkedin.com/in/bar-barina/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='pointer'
+              dangerouslySetInnerHTML={{
+                __html: getSvg('linkedin'),
+              }}
+            ></a>
+            <a
+              href='https://github.com/Bar-Barina'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='pointer'
+              dangerouslySetInnerHTML={{
+                __html: getSvg('github'),
+              }}
+            ></a>
+          </div>
         </div>
-      </div>
-      <div className='img-background'>
-        <img src={MyImg} alt='avatar' className='avatar' />
+        <div className='img-background'>
+          <img src={MyImg} alt='avatar' className='avatar' />
+        </div>
+      </section>
+      <div className='skills'>
+        <span>Tech Stack</span>
       </div>
 
-      {/* <img
-        src='https://mattfarley.ca/img/hero-devices.svg'
-        alt='avatar'
-        className='devices'
-      />
-      <div className='purple-div flex column'> */}
+      {/* <div className='purple-div flex column'> */}
       {/* <h3>Hi, I’m Bar. Nice to meet you.</h3>
         <p>
           A Front-End developer at day, and a professional wrestler at night who
